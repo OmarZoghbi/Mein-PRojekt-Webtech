72730661e9beffb686a518ac1bfae864d97fb852
OmarZoghbi <omarzoghbi93@gmail.com>
2024-11-07 13:00:57 +0100

    Initial commit

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'MJO-GAmesCOm';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+// Installationsereignis: Dateien im Cache speichern
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching assets');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Abrufereignis: Dateien aus dem Cache bereitstellen
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Update des Caches beim Aktivieren des Service Workers
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Alten Cache löschen:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
